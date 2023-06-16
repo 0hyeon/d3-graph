e0648312b4030179fc02c6ac69f1798b169963ba
0hyeon <djdjdjk2006@naver.com>
2023-06-16 13:07:48 +0900

    feat: 막대그래프

---
+++ app/Headers.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+function Headers({}: Props) {
+  return (
+    <nav className="flex space-x-4 mb-2">
+      <Link href="/" className="bg-blue-200 rounded px-4 py-2">
+        Home
+      </Link>
+      <Link href="/todos" className="bg-cyan-200 rounded px-4 py-2">
+        Graph1
+      </Link>
+    </nav>
+  );
+}
+
+export default Headers;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {
+      appDir: true,
+      esmExternals: 'loose',
+    },
+  };
+  
+  module.exports = nextConfig;
